cf9fdd6de21625c669c6264b0da49315b00856f8
sbcerqra <sarahbia09@gmail.com>
2023-02-10 23:02:28 -0300

    test feature

--- src/tests/test.posts.ts
+++ src/tests/test.posts.ts
@@ -1,27 +1,13 @@
 import authorController from "../controllers/authorController";
 import { createAuthor } from "../services/author";
 import { createPost } from "../services/posts";
-// describe('testing post service', () => {
-//     test('should create a post', () => {
-//         let cats = {
-//             title: "why cats are awesome",
-//             authorId: 2,
-//             content: "because they are",
-//             published_date: 12/10/2000,
-//             categoryId: 1,
-//         }
-//         createPost(cats)
-//         expect("") 
-//     });
-// });
-
-
-// describe('testing author service', () => {
-//     test('should add an author'),() => {
-//         let author = authorController();
-//     }
-//     expect()
-// });
+describe('testing author service', () => {
+    test('should add an author'),() => {
+        let author = { name: "sarah" }
+        createAuthor(author)
+    }
+    expect(authorController).toBeCalled()
+});
 
 test("Initial test", () => {
     expect(2).toEqual(3);
